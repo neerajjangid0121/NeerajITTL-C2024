ee8a537c50aa671f77d400be8721629c25dc5179
Neeraj Jangid <neeraj.j@intimetec.com>
2025-03-05 16:20:46 +0530

    Chapter_3_Comments_Assignment

---
+++ Chapter 3 Assignment/src/main/java/com/commentsAssignment/TumblrApiService.java
@@ -0,0 +1,39 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class TumblrApiService {
+
+    public static void fetchTumblrData(String blogName, int start, int end) {
+        int apiStart = start - 1;
+        int numPosts = end - apiStart + 1;
+
+        String apiUrl = "https://" + blogName + ".tumblr.com/api/read/json?type=photo&num=" + numPosts + "&start=" + apiStart;
+
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(apiUrl).openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder jsonResponse = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                jsonResponse.append(line);
+            }
+            reader.close();
+
+            String jsonText = jsonResponse.toString()
+                    .replace("var tumblr_api_read = ", "")
+                    .replaceAll(";$", ""); // Replace the JSON responce
+
+            TumblrApiParser.parseBlogInfo(jsonText);
+            TumblrApiParser.parseImageUrls(jsonText, start, end);
+
+        } catch (Exception e) {
+            System.out.println("Error fetching data: " + e.getMessage());
+        }
+    }
+}
