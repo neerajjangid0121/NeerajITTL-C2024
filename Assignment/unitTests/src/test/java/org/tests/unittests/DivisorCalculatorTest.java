5071eaebadf61c3a98d8415b487c6035e8ecff48
Neeraj Jangid <neeraj.j@intimetec.com>
2025-06-03 15:27:57 +0530

    Added initial test

--- Assignment/unitTests/src/test/java/org/tests/unittests/DivisorCalculatorTest.java
+++ Assignment/unitTests/src/test/java/org/tests/unittests/DivisorCalculatorTest.java
@@ -1,7 +1,6 @@
 package org.tests.unittests;
 
 import org.testng.annotations.Test;
-import org.tests.DivisorCalculator;
 
 import static org.testng.Assert.assertEquals;
 
@@ -9,15 +8,5 @@ public class DivisorCalculatorTest {
     @Test
     public void testCountDivisorsPair_ValidPair() {
         assertEquals(DivisorCalculator.countPairs(3), 1);
-    }
-
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void testCountPairs_NegativeInput() {
-        DivisorCalculator.countPairs(-5);
-    }
-
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void testCountPairs_ZeroInput() {
-        DivisorCalculator.countPairs(0);
     }
 }
