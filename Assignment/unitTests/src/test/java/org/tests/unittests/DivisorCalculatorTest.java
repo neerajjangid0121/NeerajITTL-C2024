e9ecf89c726f76a3ba597be3bb1b299593736a8c
Neeraj Jangid <neeraj.j@intimetec.com>
2025-06-03 15:53:40 +0530

    Added more tests and thier code

--- Assignment/unitTests/src/test/java/org/tests/unittests/DivisorCalculatorTest.java
+++ Assignment/unitTests/src/test/java/org/tests/unittests/DivisorCalculatorTest.java
@@ -9,4 +9,12 @@ public class DivisorCalculatorTest {
     public void testCountDivisorsPair_ValidPair() {
         assertEquals(DivisorCalculator.countPairs(3), 1);
     }
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testCountPairs_throwExceptionOnNegativeInput() {
+        DivisorCalculator.countPairs(-5);
+    }
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testCountPairs_throwExceptionOnZeroInput() {
+        DivisorCalculator.countPairs(0);
+    }
 }
